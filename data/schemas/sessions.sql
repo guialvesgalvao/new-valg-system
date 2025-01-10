307caf65e8b86048e45f5facf8c8ef476c977fc0
guialvesgalvao <guilhermeg1703@hotmail.com>
2025-01-09 21:33:52 -0300

    ✨ Feat: Update in table schema for users

--- data/schemas/sessions.sql
+++ data/schemas/sessions.sql
@@ -1,13 +1,16 @@
-CREATE TABLE
-  `bills` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) NOT NULL,
-    `amount` decimal(10, 2) NOT NULL,
-    `due_date` date NOT NULL,
-    `status` varchar(50) NOT NULL,
-    `is_generated_by_recurrence` tinyint(1) NOT NULL DEFAULT '0',
-    `user` varchar(255) NOT NULL,
-    `modified_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
-  ) ENGINE = InnoDB AUTO_INCREMENT = 16 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
+CREATE TABLE `sessions` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `user_id` INT UNSIGNED NOT NULL,
+    `acess_token` VARCHAR(255),
+    `refresh_token` VARCHAR(255),
+    `acess_token_expires_at` DATE DEFAULT NULL,
+    `refresh_token_expires_at` DATE NOT NULL,
+    `token_type` VARCHAR(255),
+    `modified_at` DATE,
+    `created_at` DATE,
+    `revoked` BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (`id`, `user_id`),
+    CONSTRAINT `fk_sessions_users` 
+        FOREIGN KEY (`user_id`) 
+        REFERENCES `users` (`id`)
+) ENGINE = InnoDB;
